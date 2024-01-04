3e453b917b9825dc3b418dcc16f50f8a22716729
jinyr1128 <jinyr1128@gmail.com>
2024-01-04 20:09:44 +0900

    feat : WebSecurityConfig

---
+++ src/main/java/com/sportsecho/common/configuration/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.sportsecho.common.configuration;
+
+import com.sportsecho.common.jwt.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity.sessionManagement(sess ->
+                sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers(publicEndPoints()).permitAll()
+                .anyRequest().authenticated());
+
+        //JwtFilter 설정
+        httpSecurity.addFilterBefore(jwtAuthorizationFilter, JwtAuthorizationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    public RequestMatcher publicEndPoints() {
+        return new OrRequestMatcher(
+            new AntPathRequestMatcher("/api")
+        );
+    }
+}
