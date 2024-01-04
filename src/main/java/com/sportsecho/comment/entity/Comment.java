c18597dde3bd9c85565af84a86ba137a086325a6
jinyr1128 <jinyr1128@gmail.com>
2024-01-04 20:05:54 +0900

    feat : Comment Entity

---
+++ src/main/java/com/sportsecho/comment/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.sportsecho.comment.entity;
+
+import com.sportsecho.common.time.TimeStamp;
+import com.sportsecho.match.entity.Match;
+import com.sportsecho.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@NoArgsConstructor
+public class Comment extends TimeStamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String content;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "match_id")
+    private Match match;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member; // 댓글을 남긴 멤버
+
+}
