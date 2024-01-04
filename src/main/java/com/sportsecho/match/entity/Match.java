47e13f86b90a88704ac68cb639722902f129eae0
jinyr1128 <jinyr1128@gmail.com>
2024-01-04 20:04:39 +0900

    feat : Match Entity

---
+++ src/main/java/com/sportsecho/match/entity/Match.java
@@ -0,0 +1,29 @@
+package com.sportsecho.match.entity;
+
+import com.sportsecho.comment.entity.Comment;
+import com.sportsecho.common.time.TimeStamp;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "match")
+@NoArgsConstructor
+public class Match extends TimeStamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String teamA;
+    private String teamB;
+    private LocalDateTime matchDateTime; // 경기 일시
+    private String location; // 경기 장소
+
+    @OneToMany(mappedBy = "match")
+    private List<Comment> comments = new ArrayList<>();
+}
