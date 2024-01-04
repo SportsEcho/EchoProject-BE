b403c48802c2f726edebb1cd6c68dadffe9c8548
jinyr1128 <jinyr1128@gmail.com>
2024-01-04 20:09:11 +0900

    feat : MemberRepository

---
+++ src/main/java/com/sportsecho/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.sportsecho.member.repository;
+
+import com.sportsecho.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
