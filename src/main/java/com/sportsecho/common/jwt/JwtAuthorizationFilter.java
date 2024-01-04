d125e7a4e98d589b28558e2f7f8283a9be350e6b
jinyr1128 <jinyr1128@gmail.com>
2024-01-04 20:07:28 +0900

    feat : JwtAuthorizationFilter

---
+++ src/main/java/com/sportsecho/common/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,35 @@
+package com.sportsecho.common.jwt;
+
+import com.sportsecho.member.service.MemberDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j(topic = "JwtAuthorizationFilter")
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final MemberDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        //TODO
+
+        filterChain.doFilter(request, response);
+    }
+}
