931e7847bbeed93ea0276662dec3eb3fcad905db
jinyr1128 <jinyr1128@gmail.com>
2024-01-04 20:04:12 +0900

    feat : Cart Entity

---
+++ src/main/java/com/sportsecho/cart/entity/Cart.java
@@ -0,0 +1,43 @@
+package com.sportsecho.cart.entity;
+
+import com.sportsecho.member.entity.Member;
+import com.sportsecho.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer productsQuantity;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Builder
+    public Cart(Integer productsQuantity){
+        this.productsQuantity = productsQuantity;
+    }
+
+    public Cart create(Integer productsQuantity){
+        return Cart.builder()
+                .productsQuantity(productsQuantity)
+                .build();
+    }
+
+    public void update(Integer productsQuantity){
+        this.productsQuantity = productsQuantity;
+    }
+}
