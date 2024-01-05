4900846a1cffebbd23e6ef60f5baf36889e36c6a
zzzzseong <jung48182@gmail.com>
2024-01-05 16:39:02 +0900

    feat(Member): MemberController 추가

---
+++ src/main/java/com/sportsecho/member/controller/MemberController.java
@@ -0,0 +1,37 @@
+package com.sportsecho.member.controller;
+
+import com.sportsecho.member.dto.MemberRequestDto;
+import com.sportsecho.member.service.MemberServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+
+    private final MemberServiceImpl memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody MemberRequestDto request) {
+        memberService.signup(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+        @RequestBody MemberRequestDto request,
+        HttpServletResponse response
+    ) {
+        memberService.login(request, response);
+
+        return ResponseEntity.ok().body(null);
+    }
+}
