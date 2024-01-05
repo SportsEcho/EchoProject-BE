01d767b92529a2b4c81af5b8494d5299118e47cd
zzzzseong <jung48182@gmail.com>
2024-01-05 12:11:10 +0900

    fix(Security): FilterChain 연결 오류 해결 - JwtAuthorizationFilter -> UsernamePasswordAuthenticationFilter

--- src/main/java/com/sportsecho/common/configuration/WebSecurityConfig.java
+++ src/main/java/com/sportsecho/common/configuration/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import com.sportsecho.common.jwt.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -28,6 +31,11 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
@@ -40,7 +48,7 @@ public class WebSecurityConfig {
                 .anyRequest().authenticated());
 
         //JwtFilter 설정
-        httpSecurity.addFilterBefore(jwtAuthorizationFilter, JwtAuthorizationFilter.class);
+        httpSecurity.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
