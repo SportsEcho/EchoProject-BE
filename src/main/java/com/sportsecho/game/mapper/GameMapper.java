ea970d9fc16f226e13c4b280e8139fd8277dddc9
jinyr1128 <jinyr1128@gmail.com>
2024-01-05 21:01:52 +0900

    feat (Game) : Mapper Class 생성 후 구성

--- src/main/java/com/sportsecho/game/mapper/GameMapper.java
+++ src/main/java/com/sportsecho/game/mapper/GameMapper.java
@@ -1,15 +1,17 @@
 package com.sportsecho.game.mapper;
 
 import com.sportsecho.game.dto.GameResponseDto;
+import com.sportsecho.game.entity.Game;
 import java.util.Map;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-
 @Mapper
 public interface GameMapper {
     GameMapper INSTANCE = Mappers.getMapper(GameMapper.class);
+
+    GameResponseDto gameToGameResponseDto(Game game);
 
     @Mapping(target = "sportType", source = "gameData.sportType")
     @Mapping(target = "teamA", source = "gameData.teamA")
