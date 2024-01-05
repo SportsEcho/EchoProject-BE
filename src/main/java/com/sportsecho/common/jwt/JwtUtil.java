44f633a11a3c8909ae48d892fec583f2b650b11a
zzzzseong <jung48182@gmail.com>
2024-01-05 12:32:25 +0900

    feat(Jwt): Jwt 사용을 위한 Util 클래스 추가

--- src/main/java/com/sportsecho/common/jwt/JwtUtil.java
+++ src/main/java/com/sportsecho/common/jwt/JwtUtil.java
@@ -1,14 +1,47 @@
 package com.sportsecho.common.jwt;
 
 
+import com.sportsecho.member.entity.MemberRole;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUtil {
 
+    private final String BEARER_PREFIX = "Bearer ";
+    private final String AUTHORIZATION_KEY = "auth";
+
+    private final Long TOKEN_TIME = 60 * 60 * 1000L;
+
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
     @Value("${jwt.secret_key}")
     private String secretKey;
 
-    //TODO
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String generateToken(String email, MemberRole role) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(email)
+                        .claim(AUTHORIZATION_KEY, role)
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+                        .setIssuedAt(date)
+                        .signWith(key, signatureAlgorithm)
+                        .compact();
+    }
 }
