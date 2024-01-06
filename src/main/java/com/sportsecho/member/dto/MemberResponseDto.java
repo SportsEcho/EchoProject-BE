25e7805b4f800b2cda7f97cd275b39f3befa0d11
zzzzseong <jung48182@gmail.com>
2024-01-06 14:16:34 +0900

    feat(Member): MemberResponseDto 추가 및 Service 인터페이스 response 수정

---
+++ src/main/java/com/sportsecho/member/dto/MemberResponseDto.java
@@ -0,0 +1,9 @@
+package com.sportsecho.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberResponseDto {
+    private String memberName;
+    private String email;
+}
--- src/main/java/com/sportsecho/member/service/MemberService.java
+++ src/main/java/com/sportsecho/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.sportsecho.member.service;
 
+import com.sportsecho.common.dto.ApiResponse;
 import com.sportsecho.member.dto.MemberRequestDto;
+import com.sportsecho.member.dto.MemberResponseDto;
 import com.sportsecho.member.entity.Member;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -14,19 +16,19 @@ public interface MemberService {
      * @param request Member 회원가입 요청 정보
      * @return Member 회원가입 정보
      * */
-    Member signup(MemberRequestDto request);
+    ApiResponse<MemberResponseDto> signup(MemberRequestDto request);
 
     /**
      * Member 로그인
      * @param response Jwt 토큰을 담아서 전달하기 위한 HttpServletResponse
      * @param request Member 로그인 요청 정보
      * */
-    void login(MemberRequestDto request, HttpServletResponse response);
+    ApiResponse<MemberResponseDto> login(MemberRequestDto request, HttpServletResponse response);
 
     /**
      * Member 삭제
      * @param member 삭제할 Member 객체
      * @return 삭제된 Member 정보
      * */
-    Member deleteMember(Member member);
+    ApiResponse<MemberResponseDto> deleteMember(Member member);
 }
