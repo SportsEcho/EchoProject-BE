5a504e537d0cb82eaa6f370c88e0b1121460e508
zzzzseong <jung48182@gmail.com>
2024-01-06 15:46:05 +0900

    feat(Member): MemberMapper 추가

--- src/main/java/com/sportsecho/member/dto/MemberResponseDto.java
+++ src/main/java/com/sportsecho/member/dto/MemberResponseDto.java
@@ -1,8 +1,10 @@
 package com.sportsecho.member.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class MemberResponseDto {
     private String memberName;
     private String email;
