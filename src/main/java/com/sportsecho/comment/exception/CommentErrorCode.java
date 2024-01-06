e8bc00183d2157b9a66dd28ee284b23dec934ff0
jinyr1128 <jinyr1128@gmail.com>
2024-01-06 15:47:36 +0900

    fix (Global) : CommentErrorCode

--- src/main/java/com/sportsecho/comment/exception/CommentErrorCode.java
+++ src/main/java/com/sportsecho/comment/exception/CommentErrorCode.java
@@ -1,12 +1,13 @@
 package com.sportsecho.comment.exception;
 
+import com.sportsecho.global.exception.BaseErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
-public enum CommentErrorCode {
+public enum CommentErrorCode implements BaseErrorCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다"),
     UNAUTHORIZED_COMMENT_ACCESS(HttpStatus.UNAUTHORIZED, "댓글 접근 권한이 없습니다");
 
