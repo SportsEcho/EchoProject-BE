e0ca7db041f438be12dbbdf011027433893c382c
jinyr1128 <jinyr1128@gmail.com>
2024-01-06 16:18:38 +0900

    feat (Comment) : CommentErrorCode 추가

--- src/main/java/com/sportsecho/comment/exception/CommentErrorCode.java
+++ src/main/java/com/sportsecho/comment/exception/CommentErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CommentErrorCode implements BaseErrorCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다"),
+    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "게임을 찾을 수 없습니다"),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다"),
     UNAUTHORIZED_COMMENT_ACCESS(HttpStatus.UNAUTHORIZED, "댓글 접근 권한이 없습니다");
+
 
 
     private final HttpStatus status;
