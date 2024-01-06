59de5a102d3383da4ef1956488c5d6cd697ea4d7
jinyr1128 <jinyr1128@gmail.com>
2024-01-06 15:19:59 +0900

    feat (Comment) : CommentException

--- src/main/java/com/sportsecho/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sportsecho/global/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sportsecho.global.handler;
 
+import com.sportsecho.comment.exception.CommentException;
 import com.sportsecho.global.exception.ExternalApiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_GATEWAY)
                 .body(e.getMessage());
+    }
+    @ExceptionHandler(CommentException.class)
+    public ResponseEntity<Object> handleCommentException(CommentException e) {
+        HttpStatus status = e.getErrorCode().getStatus();
+        String message = e.getErrorCode().getMsg();
+
+        return ResponseEntity
+            .status(status)
+            .body(message);
     }
 }
