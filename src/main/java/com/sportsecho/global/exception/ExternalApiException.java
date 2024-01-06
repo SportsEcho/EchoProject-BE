72b863b5d46ec28efd797a4ae90cca4479df60af
jinyr1128 <jinyr1128@gmail.com>
2024-01-06 15:47:51 +0900

    fix (Global) : ExternalApiException

--- src/main/java/com/sportsecho/global/exception/ExternalApiException.java
+++ src/main/java/com/sportsecho/global/exception/ExternalApiException.java
@@ -1,11 +1,11 @@
 package com.sportsecho.global.exception;
 
 public class ExternalApiException extends GlobalException {
-    public ExternalApiException(ErrorCode errorCode, String message) {
+    public ExternalApiException(BaseErrorCode errorCode, String message) {
         super(errorCode, message);
     }
 
-    public ExternalApiException(ErrorCode errorCode) {
+    public ExternalApiException(BaseErrorCode errorCode) {
         super(errorCode);
     }
 }
