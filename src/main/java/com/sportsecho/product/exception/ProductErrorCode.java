0d25b350c8a378949b7cb7478bef2b994ea0d9de
문정현 <foqlzm12345@gmail.com>
2024-01-08 22:53:39 +0900

    refactor(Product) : product refactor GlobalExection 예외 처리 serviceImpl 주입 방식 통일

--- src/main/java/com/sportsecho/product/exception/ProductErrorCode.java
+++ src/main/java/com/sportsecho/product/exception/ProductErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ProductErrorCode implements BaseErrorCode {
-    NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다");
+    NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다"),
+    NO_AUTHORIZATION(HttpStatus.FORBIDDEN, "수정 권한이 없습니다.");
+    ;
 
     private final HttpStatus status;
     private final String msg;
