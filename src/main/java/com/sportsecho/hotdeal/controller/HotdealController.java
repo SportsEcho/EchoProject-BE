03d9e7f96224cba80df4b09bf09be98b48ac0450
문정현 <foqlzm12345@gmail.com>
2024-01-08 16:23:59 +0900

    fix(Product) : Hotdeal 목록 조회 Pageable Url 받아서 생성

--- src/main/java/com/sportsecho/hotdeal/controller/HotdealController.java
+++ src/main/java/com/sportsecho/hotdeal/controller/HotdealController.java
@@ -13,7 +13,9 @@ import org.apache.coyote.Request;
 import org.hibernate.annotations.Fetch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -47,9 +50,15 @@ public class HotdealController {
         );
     }
 
-    @GetMapping
-    public ResponseEntity<ApiResponse<List<HotdealResponseDto>>> getHotdealList(Pageable pageable) {
+    @GetMapping("products/{productId}/hotdeals")
+    public ResponseEntity<ApiResponse<List<HotdealResponseDto>>> getHotdealList(
+        @PathVariable Long productId,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "startDay,asc") String sort
+    ) {
 
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sort.split(",")));
         List<HotdealResponseDto> responseDtoList = hotdealService.getHotdealList(pageable);
 
         return ResponseEntity.status(HttpStatus.OK).body(
