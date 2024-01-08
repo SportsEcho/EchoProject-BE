b1113139a01c44e7337591a26f81cddac8736e64
jinyr1128 <jinyr1128@gmail.com>
2024-01-08 13:28:53 +0900

    feat (game) : Ver.관리 추가 Service

---
+++ src/main/java/com/sportsecho/game/service/GameService.java
@@ -0,0 +1,8 @@
+package com.sportsecho.game.service;
+
+import com.sportsecho.game.dto.GameResponseDto;
+import java.util.List;
+
+public interface GameService {
+    List<GameResponseDto> getGamesBySport(String sportType);
+}
