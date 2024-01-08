c2ec7653075b87d031b9552ce2de2ba35a13c07e
jinyr1128 <jinyr1128@gmail.com>
2024-01-08 13:28:29 +0900

    fix (game) : Ver.관리 추가 Controller

--- src/main/java/com/sportsecho/game/controller/GameController.java
+++ src/main/java/com/sportsecho/game/controller/GameController.java
@@ -2,11 +2,12 @@ package com.sportsecho.game.controller;
 
 import com.sportsecho.comment.dto.CommentResponseDto;
 import com.sportsecho.comment.service.CommentService;
-import com.sportsecho.common.dto.ApiResponse;
 import com.sportsecho.game.dto.GameResponseDto;
 import com.sportsecho.game.service.GameService;
+import com.sportsecho.game.service.GameServiceImplV1;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +23,7 @@ public class GameController {
     private final CommentService commentService;
 
     @Autowired
-    public GameController(GameService gameService, CommentService commentService) {
+    public GameController(@Qualifier("gameServiceImplV1")GameService gameService, CommentService commentService) {
         this.gameService = gameService;
         this.commentService = commentService;
     }
