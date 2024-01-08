d3b621004b29a077f12c4df1b559c81c00fa4cb9
zzzzseong <jung48182@gmail.com>
2024-01-08 12:55:27 +0900

    feat(Jwt): JwtErrorCode 추가 및 JwtUtil에 적용

--- src/main/java/com/sportsecho/common/jwt/exception/JwtErrorCode.java
+++ src/main/java/com/sportsecho/common/jwt/exception/JwtErrorCode.java
@@ -12,7 +12,11 @@ public enum JwtErrorCode implements BaseErrorCode {
     UNSUPPORTED_JWT_EXCEPTION(HttpStatus.UNAUTHORIZED, "Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다."),
     SIGNATURE_EXCEPTION(HttpStatus.UNAUTHORIZED, "Invalid JWT signature, 유효하지 않는 JWT 서명 입니다."),
     ILLEGAL_ARGUMENT_EXCEPTION(HttpStatus.UNAUTHORIZED, "JWT claims is empty, 잘못된 JWT 토큰 입니다."),
-    EXPIRED_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED, "Expired JWT token, 만료된 JWT token 입니다.");
+    EXPIRED_TOKEN_EXCEPTION(HttpStatus.UNAUTHORIZED, "Expired JWT token, 만료된 JWT token 입니다."),
+
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "갱신토큰이 존재하지 않습니다.");
+
+    ;
 
     private final HttpStatus status;
     private final String msg;
