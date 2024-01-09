33d4946395f736347c6e432a2c0ef52d33dc6093
zzzzseong <jung48182@gmail.com>
2024-01-09 14:02:15 +0900

    feat(Member): findByKakaoId() Repository 메서드 추가

--- src/main/java/com/sportsecho/member/repository/MemberRepository.java
+++ src/main/java/com/sportsecho/member/repository/MemberRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByKakaoId(Long kakaoId);
 }
