70b2adfa6d744c17f56815ab3e75786e17e4e577
jinyr1128 <jinyr1128@gmail.com>
2024-01-10 16:00:30 +0900

    fix (Game) : api 반환에 맞춰 변화

--- src/main/java/com/sportsecho/game/controller/GameController.java
+++ src/main/java/com/sportsecho/game/controller/GameController.java
@@ -1,39 +1,33 @@
 package com.sportsecho.game.controller;
 
-import com.sportsecho.comment.dto.CommentResponseDto;
-import com.sportsecho.comment.service.CommentService;
-import com.sportsecho.comment.service.CommentServiceImplV1;
 import com.sportsecho.game.dto.GameResponseDto;
 import com.sportsecho.game.service.GameService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/games")
 public class GameController {
     private final GameService gameService;
-    private final CommentService commentService;
 
     @Autowired
-    public GameController(@Qualifier("gameServiceImplV1")GameService gameService,
-                          @Qualifier("commentServiceImplV1") CommentService commentService) {
+    public GameController(@Qualifier("gameServiceImplV1") GameService gameService) {
         this.gameService = gameService;
-        this.commentService = commentService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<GameResponseDto>> getGamesBySport(@RequestParam String sportType) {
-        List<GameResponseDto> games = gameService.getGamesBySport(sportType);
-        return ResponseEntity.status(HttpStatus.OK).body(games);
+    @GetMapping("/by-league")
+    public ResponseEntity<List<GameResponseDto>> getGamesByDateAndLeague(
+        @RequestParam String date,
+        @RequestParam String league
+    ) {
+        List<GameResponseDto> games = gameService.getGamesByDateAndLeague(date, league);
+        return ResponseEntity.ok(games);
     }
 }
-
-
