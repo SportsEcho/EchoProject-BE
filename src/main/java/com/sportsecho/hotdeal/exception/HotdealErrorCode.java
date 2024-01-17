912cdcd77e3269d38a11e82596943457c4749f9b
문정현 <foqlzm12345@gmail.com>
2024-01-17 19:01:16 +0900

    #73 feat(Hotdeal) : 구매상품 구분을 위한 role 추가 및 error코드 추가
    Co-authored-by: zomeong <legojhk@gmail.com>

--- src/main/java/com/sportsecho/hotdeal/exception/HotdealErrorCode.java
+++ src/main/java/com/sportsecho/hotdeal/exception/HotdealErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum HotdealErrorCode implements BaseErrorCode {
     NOT_FOUND_HOTDEAL(HttpStatus.NOT_FOUND, "핫딜을 찾을 수 없습니다"),
     LACK_DEAL_QUANTITY(HttpStatus.BAD_REQUEST, "구매 하고자 하는 갯수가 남은 한정수량 보다 많습니다"),
-    NO_AUTHORIZATION(HttpStatus.FORBIDDEN, "수정 권한이 없습니다.");
+    NO_AUTHORIZATION(HttpStatus.FORBIDDEN, "수정 권한이 없습니다."),
+    SOLD_OUT(HttpStatus.NOT_FOUND, "품절된 상품입니다.");
 
     private final HttpStatus status;
     private final String msg;
