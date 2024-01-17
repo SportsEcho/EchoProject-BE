4d6e818dffc2e3a5ef1824796516f7ee0c2044fd
Kim Jihyun <legojhk@gmail.com>
2024-01-17 21:31:24 +0900

    refactor(cart) : resopnseDto id 추가, fetch type 설정

--- src/main/java/com/sportsecho/memberProduct/entity/MemberProduct.java
+++ src/main/java/com/sportsecho/memberProduct/entity/MemberProduct.java
@@ -2,7 +2,13 @@ package com.sportsecho.memberProduct.entity;
 
 import com.sportsecho.member.entity.Member;
 import com.sportsecho.product.entity.Product;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,11 +27,11 @@ public class MemberProduct {
 
     private int productsQuantity;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
