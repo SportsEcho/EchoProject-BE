f1e38f4e299347867288cda40b1533137a488c23
문정현 <foqlzm12345@gmail.com>
2024-01-17 19:01:38 +0900

    #73 feat(Hotdeal) : 구매상품 구분을 위한 role 추가 및 error코드 추가
    Co-authored-by: zomeong <legojhk@gmail.com>

--- src/main/java/com/sportsecho/purchaseProduct/entity/PurchaseProduct.java
+++ src/main/java/com/sportsecho/purchaseProduct/entity/PurchaseProduct.java
@@ -34,6 +34,8 @@ public class PurchaseProduct {
     @JoinColumn(name = "product_id")
     private Product product;
 
+    private ProductRole role;
+
     @Builder// 임시로 쓰겠슴다
     public PurchaseProduct(Purchase purchase, Product product, int productsQuantity, ProductRole role) {
         this.purchase = purchase;
