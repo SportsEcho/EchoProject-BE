3ffd9963eaf6d6a324c0e2180a825e18e0765aa0
Kim Jihyun <legojhk@gmail.com>
2024-01-18 17:36:32 +0900

    feat(purchase) : cancelPurchase API 생성, purchase API 재고 감소 로직 추가

--- src/main/java/com/sportsecho/purchase/service/PurchaseServiceImplV1.java
+++ src/main/java/com/sportsecho/purchase/service/PurchaseServiceImplV1.java
@@ -4,12 +4,14 @@ import com.sportsecho.global.exception.GlobalException;
 import com.sportsecho.member.entity.Member;
 import com.sportsecho.memberProduct.entity.MemberProduct;
 import com.sportsecho.memberProduct.repository.MemberProductRepository;
+import com.sportsecho.product.entity.Product;
 import com.sportsecho.purchase.dto.PurchaseRequestDto;
 import com.sportsecho.purchase.dto.PurchaseResponseDto;
 import com.sportsecho.purchase.entity.Purchase;
 import com.sportsecho.purchase.exception.PurchaseErrorCode;
 import com.sportsecho.purchase.mapper.PurchaseMapper;
 import com.sportsecho.purchase.repository.PurchaseRepository;
+import com.sportsecho.purchaseProduct.entity.ProductRole;
 import com.sportsecho.purchaseProduct.entity.PurchaseProduct;
 import com.sportsecho.purchaseProduct.repository.PurchaseProductRepository;
 import java.util.List;
@@ -38,8 +40,8 @@ public class PurchaseServiceImplV1 implements PurchaseService {
             throw new GlobalException(PurchaseErrorCode.EMPTY_CART);
         }
 
-        // 재고 확인
-        checkStock(memberProductList);
+        // 재고 확인 후 차감
+        updateStock(memberProductList);
 
         // 구매 인스턴스 생성
         Purchase purchase = PurchaseMapper.INSTANCE.toEntity(requestDto, member);
@@ -47,8 +49,6 @@ public class PurchaseServiceImplV1 implements PurchaseService {
 
         // 구매 정보와 장바구니 상품 리스트로 purchaseProduct 엔티티 리스트 생성
         List<PurchaseProduct> purchaseProductList = createPList(memberProductList, purchase);
-        purchaseProductRepository.saveAll(purchaseProductList);
-        purchase.getPurchaseProductList().addAll(purchaseProductList);
 
         // 총 금액 업데이트
         purchase.updateTotalPrice(calTotalPrice(purchaseProductList));
@@ -66,12 +66,25 @@ public class PurchaseServiceImplV1 implements PurchaseService {
 
         List<Purchase> purchaseList = purchaseRepository.findByMemberId(member.getId());
         if (purchaseList.isEmpty()) {
-            throw new GlobalException(PurchaseErrorCode.EMPTY_PURCHASE_LIST);
+            throw new GlobalException(PurchaseErrorCode.NOT_FOUND_PURCHASE);
         }
 
         return purchaseList.stream()
             .map(Purchase::createResponseDto)
             .toList();
+    }
+
+    @Override
+    @Transactional
+    public void cancelPurchase(Long purchaseId, Member member) {
+
+        Purchase purchase = purchaseRepository.findById(purchaseId).orElseThrow(
+            () -> new GlobalException(PurchaseErrorCode.NOT_FOUND_PURCHASE));
+        checkMember(purchase, member);
+        updateStock(purchase);
+
+        purchaseProductRepository.deleteByPurchaseId(purchaseId);
+        purchaseRepository.deleteById(purchaseId);
     }
 
     private int calTotalPrice(List<PurchaseProduct> purchaseProductList) {
@@ -83,22 +96,46 @@ public class PurchaseServiceImplV1 implements PurchaseService {
 
     private List<PurchaseProduct> createPList(List<MemberProduct> memberProductList,
         Purchase purchase) {
-        return memberProductList.stream()
+
+        List<PurchaseProduct> pList = memberProductList.stream()
             .map(memberProduct -> PurchaseProduct.builder()
                 .purchase(purchase)
                 .product(memberProduct.getProduct())
                 .productsQuantity(memberProduct.getProductsQuantity())
+                .role(ProductRole.PRODUCT)
                 .build())
             .toList();
+
+        purchaseProductRepository.saveAll(pList);
+        purchase.getPurchaseProductList().addAll(pList);
+
+        return pList;
     }
 
-    private void checkStock(List<MemberProduct> memberProductList) {
-        memberProductList.stream()
-            .filter(memberProduct -> memberProduct.getProductsQuantity() >
-                memberProduct.getProduct().getQuantity())
-            .findFirst()
-            .ifPresent(memberProduct -> {
+    private void updateStock(Purchase purchase) {
+        List<PurchaseProduct> purchaseProductList = purchase.getPurchaseProductList();
+        for (PurchaseProduct purchaseProduct : purchaseProductList) {
+            Product product = purchaseProduct.getProduct();
+            product.increaseQuantity(product.getQuantity());
+        }
+    }
+
+    private void updateStock(List<MemberProduct> memberProductList) {
+
+        for (MemberProduct memberProduct : memberProductList) {
+            Product product = memberProduct.getProduct();
+
+            if (memberProduct.getProductsQuantity() > product.getQuantity()) {
                 throw new GlobalException(PurchaseErrorCode.OUT_OF_STOCK);
-            });
+            } else {
+                product.decreaseQuantity(memberProduct.getProductsQuantity());
+            }
+        }
+    }
+
+    private void checkMember(Purchase purchase, Member member) {
+        if (!purchase.getMember().getId().equals(member.getId())) {
+            throw new GlobalException(PurchaseErrorCode.ACCESS_DENIED);
+        }
     }
 }
