7afb2864516bbf6729ad26a72aad6605cdbd4e9e
Kim Jihyun <legojhk@gmail.com>
2024-01-19 19:07:22 +0900

    refactor(purchase) : toResponseDto 매퍼로 변경

--- src/main/java/com/sportsecho/purchase/entity/Purchase.java
+++ src/main/java/com/sportsecho/purchase/entity/Purchase.java
@@ -2,9 +2,7 @@ package com.sportsecho.purchase.entity;
 
 import com.sportsecho.common.time.TimeStamp;
 import com.sportsecho.member.entity.Member;
-import com.sportsecho.purchase.dto.PurchaseResponseDto;
 import com.sportsecho.purchaseProduct.entity.PurchaseProduct;
-import com.sportsecho.purchaseProduct.mapper.PurchaseProductMapper;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -49,21 +47,5 @@ public class Purchase extends TimeStamp {
 
     public void updateTotalPrice(int totalPrice) {
         this.totalPrice = totalPrice;
-    }
-
-    // 연관 관계가 복잡하여 mapper를 사용하지 않음.. 추후 수정 가능성 있음..
-    public PurchaseResponseDto createResponseDto() {
-        return PurchaseResponseDto.builder()
-            .id(this.id)
-            .totalPrice(this.totalPrice)
-            .address(this.address)
-            .phone(this.phone)
-            .purchaseDate(this.getCreatedAt())
-            .responseDtoList(
-                this.purchaseProductList.stream()
-                    .map(PurchaseProductMapper.INSTANCE::toResponseDto)
-                    .toList()
-            )
-            .build();
     }
 }
