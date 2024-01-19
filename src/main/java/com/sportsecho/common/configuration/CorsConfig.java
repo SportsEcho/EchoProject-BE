b8668202742dd378770c3ecc3bac399eb54b1ea7
zzzzseong <jung48182@gmail.com>
2024-01-19 12:21:51 +0900

    feat(Common): Cors allow uri에 port 추가

--- src/main/java/com/sportsecho/common/configuration/CorsConfig.java
+++ src/main/java/com/sportsecho/common/configuration/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://sportsecho.life");
+            .allowedOrigins("http://sportsecho.life:3000");
     }
 }
 
