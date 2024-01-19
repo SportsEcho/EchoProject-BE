5e1949f0fc4e2529d91d4a2c5151ee29cfb5ce25
Kim Jihyun <legojhk@gmail.com>
2024-01-19 14:53:15 +0900

    test(purchase) : 테스트 데이터 변경

--- src/test/java/com/sportsecho/purchase/PurchaseTest.java
+++ src/test/java/com/sportsecho/purchase/PurchaseTest.java
@@ -3,5 +3,5 @@ package com.sportsecho.purchase;
 public interface PurchaseTest {
 
     String TEST_ADDRESS = "스포츠시 에코동";
-    String TEST_PHONE = "010-1234-5678";
+    String TEST_PHONE = "01012345678";
 }
