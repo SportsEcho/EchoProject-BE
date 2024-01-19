b3f61dbe3c688d13ec5c4d0f21ca732236fac67a
문정현 <foqlzm12345@gmail.com>
2024-01-19 15:51:41 +0900

    chore(Game) : 시간 간격이 아닌 정각 수행으로 변경

--- src/main/java/com/sportsecho/game/scheduler/GameScheduler.java
+++ src/main/java/com/sportsecho/game/scheduler/GameScheduler.java
@@ -53,7 +53,7 @@ public class GameScheduler {
     }
 
 //    @Scheduled(fixedRate = 900000) // 매 15분마다 실행 (900000ms = 30분) = 하루 96번 호출
-    @Scheduled(fixedRate = 3600000) // 과금 이슈로 최종 베포 전까지 한 시간 마다 수행
+    @Scheduled(cron = "0 0 * * * ?") // 과금 이슈로 최종 베포 전까지 한 시간 마다 수행
     public void updateTodayGame() throws IOException, InterruptedException, JSONException {
 
         LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
