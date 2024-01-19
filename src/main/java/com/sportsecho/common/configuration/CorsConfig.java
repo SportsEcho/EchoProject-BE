08c4ea3476692c35ba9ac5d81f5508872c891b75
zzzzseong <jung48182@gmail.com>
2024-01-19 16:33:09 +0900

    feat(Common): CorsConfig Header 허용 코드 추가

--- src/main/java/com/sportsecho/common/configuration/CorsConfig.java
+++ src/main/java/com/sportsecho/common/configuration/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sportsecho.common.configuration;
 
+import com.sportsecho.common.jwt.JwtUtil;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://sportsecho.life:3000");
+            .allowedOrigins("http://sportsecho.life:3000")
+            .exposedHeaders(JwtUtil.AUTHORIZATION_HEADER, JwtUtil.REFRESH_AUTHORIZATION_HEADER);
     }
 }
 
