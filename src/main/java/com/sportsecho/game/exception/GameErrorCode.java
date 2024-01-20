7cfb4d3f8acae0846f5c2886e125161289082172
zzzzseong <jung48182@gmail.com>
2024-01-20 15:55:01 +0900

    feat(Game): Game 단건 조회 메서드 추가 - GAME_NOT_FOUND ErrorCode 추가

--- src/main/java/com/sportsecho/game/exception/GameErrorCode.java
+++ src/main/java/com/sportsecho/game/exception/GameErrorCode.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum GameErrorCode implements BaseErrorCode {
     EXTERNAL_API_ERROR(HttpStatus.BAD_GATEWAY, "외부 API 오류입니다"),
-    INVALID_API_RESPONSE(HttpStatus.BAD_REQUEST, "유효하지 않은 API 응답입니다");
+    INVALID_API_RESPONSE(HttpStatus.BAD_REQUEST, "유효하지 않은 API 응답입니다"),
+
+    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "게임을 찾을 수 없습니다")
+
+    ;
 
     private final HttpStatus status;
     private final String msg;
