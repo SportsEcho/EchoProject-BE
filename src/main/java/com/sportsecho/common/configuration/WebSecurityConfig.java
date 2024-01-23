e32089f6bfba409ee2c097856356b1e8425ab260
zzzzseong <jung48182@gmail.com>
2024-01-24 01:43:21 +0900

    feat(Config): Security public endpoint 추가

--- src/main/java/com/sportsecho/common/configuration/WebSecurityConfig.java
+++ src/main/java/com/sportsecho/common/configuration/WebSecurityConfig.java
@@ -75,6 +75,7 @@ public class WebSecurityConfig {
             new AntPathRequestMatcher("/api/games/details/**"),
 
             new AntPathRequestMatcher("/api/products"),
+            new AntPathRequestMatcher("/api/products/*"),
 
             //웹소켓 endpoint
             new AntPathRequestMatcher("/websocket"),
