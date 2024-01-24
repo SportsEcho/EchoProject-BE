f4299670729afc5a3058345443ed3a1a1a01cad6
zzzzseong <jung48182@gmail.com>
2024-01-24 21:04:52 +0900

    document(Product): ProductService interface 주석 추가

--- src/main/java/com/sportsecho/product/service/ProductService.java
+++ src/main/java/com/sportsecho/product/service/ProductService.java
@@ -8,14 +8,38 @@ import org.springframework.data.domain.Pageable;
 
 public interface ProductService {
 
-    ProductResponseDto createProduct(ProductRequestDto requestDto, Member member);
+    /**
+     * Product 생성 - ADMIN 권한이 있는 사용자만 생성 가능
+     * @param requestDto Product 생성 요청 정보
+     * @return 생성된 Product 정보 반환
+     * */
+    ProductResponseDto createProduct(ProductRequestDto requestDto);
 
+    /**
+     * Product 단건 조회
+     * @param productId 단건 조회할 Product의 ID
+     * @return 단건 Product 정보 반환
+     * */
     ProductResponseDto getProduct(Long productId);
 
+    /**
+     * Product 페이지 조회
+     * @param pageable 페이지 page, size 정보를 담은 Pegeable 객체
+     * @return 페이지 Product 정보 반환
+     * */
     List<ProductResponseDto> getProductListWithPagiNation(Pageable pageable);
 
-    ProductResponseDto updateProduct(Member member, Long productId, ProductRequestDto requestDto);
+    /**
+     * Product 수정 - ADMIN 권한이 있는 사용자만 수정 가능
+     * @param productId 수정할 Product의 ID
+     * @param requestDto Product 수정 요청 정보
+     * */
+    ProductResponseDto updateProduct(Long productId, ProductRequestDto requestDto);
 
-    void deleteProduct(Member member, Long productId);
+    /**
+     * Product 삭제 - ADMIN 권한이 있는 사용자만 삭제 가능
+     * @param productId 삭제할 Product의 ID
+     * */
+    void deleteProduct(Long productId);
 
 }
