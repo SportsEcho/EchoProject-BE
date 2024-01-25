b26238d87b037de692d9d9a7656ad0a82fc25ee2
Kim Jihyun <legojhk@gmail.com>
2024-01-25 22:28:11 +0900

    refactor(cart) : findByMemberId JPQL 변경

--- src/main/java/com/sportsecho/memberProduct/repository/MemberProductRepository.java
+++ src/main/java/com/sportsecho/memberProduct/repository/MemberProductRepository.java
@@ -4,13 +4,15 @@ import com.sportsecho.memberProduct.entity.MemberProduct;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberProductRepository extends JpaRepository<MemberProduct, Long> {
 
     Optional<MemberProduct> findByProductIdAndMemberId(Long productId, Long memberId);
 
-
-    List<MemberProduct> findByMemberId(Long id);
+    @Query("SELECT mp FROM MemberProduct mp JOIN FETCH mp.product p JOIN FETCH p.productImageList WHERE mp.member.id = :memberId")
+    List<MemberProduct> findByMemberId(@Param("memberId") Long memberId);
 
     void deleteAllByMemberId(Long memberId);
 }
