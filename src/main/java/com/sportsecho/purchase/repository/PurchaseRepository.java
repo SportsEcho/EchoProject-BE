63b9e13192ab30a82e43d30b6861f915abc89602
Kim Jihyun <legojhk@gmail.com>
2024-01-25 22:58:32 +0900

    refactor(purchase) : findByMemberId JPQL 변경

--- src/main/java/com/sportsecho/purchase/repository/PurchaseRepository.java
+++ src/main/java/com/sportsecho/purchase/repository/PurchaseRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 
-    @Query("SELECT p FROM Purchase p WHERE p.member.id = :memberId ORDER BY p.createdAt DESC")
+    @Query("SELECT p FROM Purchase p JOIN FETCH p.purchaseProductList pp JOIN FETCH pp.product WHERE p.member.id = :memberId ORDER BY p.createdAt DESC")
     List<Purchase> findByMemberId(@Param("memberId") Long memberId);
 }
