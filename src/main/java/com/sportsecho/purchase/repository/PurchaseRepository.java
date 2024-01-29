ef5a8ae6a6ec939c17e3594aae87df4ea2c6d0fc
Kim Jihyun <legojhk@gmail.com>
2024-01-29 19:00:21 +0900

    refactor(purchase) : findByMemberId 엔티티 그래프로 변경

--- src/main/java/com/sportsecho/purchase/repository/PurchaseRepository.java
+++ src/main/java/com/sportsecho/purchase/repository/PurchaseRepository.java
@@ -1,13 +1,15 @@
 package com.sportsecho.purchase.repository;
 
 import com.sportsecho.purchase.entity.Purchase;
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 
-    @Query("SELECT p FROM Purchase p JOIN FETCH p.purchaseProductList pp JOIN FETCH pp.product WHERE p.member.id = :memberId ORDER BY p.createdAt DESC")
+    @EntityGraph(value = "graph.Purchase", type = EntityGraph.EntityGraphType.FETCH)
+    @Query("SELECT p FROM Purchase p WHERE p.member.id = :memberId ORDER BY p.createdAt DESC")
     List<Purchase> findByMemberId(@Param("memberId") Long memberId);
 }
