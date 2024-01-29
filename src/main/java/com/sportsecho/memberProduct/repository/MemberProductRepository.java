0bf242fb48a767fc01baa1bb9d80e50a4cb9a501
Kim Jihyun <legojhk@gmail.com>
2024-01-29 18:12:48 +0900

    refactor(cart) : findByMemberId 엔티티 그래프로 변경

--- src/main/java/com/sportsecho/memberProduct/repository/MemberProductRepository.java
+++ src/main/java/com/sportsecho/memberProduct/repository/MemberProductRepository.java
@@ -3,16 +3,15 @@ package com.sportsecho.memberProduct.repository;
 import com.sportsecho.memberProduct.entity.MemberProduct;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface MemberProductRepository extends JpaRepository<MemberProduct, Long> {
 
     Optional<MemberProduct> findByProductIdAndMemberId(Long productId, Long memberId);
 
-    @Query("SELECT mp FROM MemberProduct mp JOIN FETCH mp.product p JOIN FETCH p.productImageList WHERE mp.member.id = :memberId")
-    List<MemberProduct> findByMemberId(@Param("memberId") Long memberId);
+    @EntityGraph(value = "graph.MemberProduct", type = EntityGraph.EntityGraphType.FETCH)
+    List<MemberProduct> findByMemberId(Long memberId);
 
     void deleteAllByMemberId(Long memberId);
 }
