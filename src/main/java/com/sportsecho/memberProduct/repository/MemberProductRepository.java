13d8c4490865bb3f4cbd5f2d1eb9170327fd5d7b
zzzzseong <jung48182@gmail.com>
2024-01-31 19:19:51 +0900

    feat(MemberProduct): MemberProductRepository OPTIMISTIC Lock 적용 find함수 추가

--- src/main/java/com/sportsecho/memberProduct/repository/MemberProductRepository.java
+++ src/main/java/com/sportsecho/memberProduct/repository/MemberProductRepository.java
@@ -1,14 +1,22 @@
 package com.sportsecho.memberProduct.repository;
 
 import com.sportsecho.memberProduct.entity.MemberProduct;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberProductRepository extends JpaRepository<MemberProduct, Long> {
 
     Optional<MemberProduct> findByProductIdAndMemberId(Long productId, Long memberId);
 
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("select mp from MemberProduct mp join fetch mp.product p where mp.member.id = :memberId")
+    List<MemberProduct> findByMemberIdJoinProductWithOptLock(@Param("memberId") Long memberId);
 
     List<MemberProduct> findByMemberId(Long id);
 
